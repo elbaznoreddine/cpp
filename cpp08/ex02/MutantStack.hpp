8f0a7da63447aeb4b517990222756c3e84c1bbf2
Noreddine El Baz <noel-baz@e1r9p15.1337.ma>
2026-04-09 11:58:08 +0100

    cpp08

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <stack>
+#include <list>
+
+
+template <class T>
+class MutantStack : public std::stack<T>
+{
+    public :
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator begin()
+        {
+            return this->c.begin();
+        }
+        iterator end()
+        {
+            return this->c.end();
+        }
+};
